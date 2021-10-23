f4bdeec4018b5749b53f1584d503e5d80326feac
Druidzao <51032896+Druidzao@users.noreply.github.com>
2021-10-23 19:11:26 -0300

    Aula dia 23/09/2021

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/teste/{nome}', function ($nome){
+    return "<h1>Olá".$nome."!</h1>";
+});
 
+Route::get('/soma/{n1}/{n2}', function ($n1,$n2){
+    return "<h1> A soma é: ".$n1+$n2."!</h1>";
+});
